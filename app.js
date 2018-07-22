8c76c41a570fa7294d79119cb0fb77cfc531169c
mazgagan <mazgagan@gmail.com>
2018-07-21 22:56:21 -0700

    error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//import { error } from 'util';
+
 /* used to spin up the express application 
 which will make handling requests easier*/
 
@@ -6,10 +8,30 @@ const express = require('express');
 //spin up the express application
 const app = express();
 
+const morgan = require('morgan');
+
 const productRoutes = require('./Api/routes/products');
 const orderRoutes = require('./Api/routes/orders');
+
 //an incoming request has to go through app.use
+app.use(morgan('dev'));
+// incoming routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//to handle errors or handle invlaid requests.
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 module.exports = app;
