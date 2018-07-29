d9de37c4433229605727428c85deab914a82a870
mazgagan <mazgagan@gmail.com>
2018-07-29 12:28:40 -0700

    managing different orders

---
+++ Api/models/order.js
@@ -0,0 +1,11 @@
+/*
+This file is to define how an order should look like for mongodb.
+*/
+const mongoose = require('mongoose');
+const orderSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, default: 1 }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
